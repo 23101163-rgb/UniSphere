7be2100f4de8d10cee577fd74f2454b383ad28c5
Adika2003 <adikasultana88m@gmail.com>
2026-04-12 06:05:53 +0600

    Add materials admin.py

--- unisphere_project/materials/admin.py
+++ unisphere_project/materials/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import StudyMaterial, MaterialRating
+
+@admin.register(StudyMaterial)
+class StudyMaterialAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course_name', 'semester', 'uploaded_by', 'is_approved', 'created_at']
+    list_filter = ['is_approved', 'semester', 'course_name']
+    search_fields = ['title', 'course_name', 'topic']
+
+@admin.register(MaterialRating)
+class MaterialRatingAdmin(admin.ModelAdmin):
+    list_display = ['material', 'user', 'score', 'created_at']
+

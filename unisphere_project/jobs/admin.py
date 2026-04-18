830f95f49f0c4dbfbc59a70a5700fe3d40bdfd81
Adika2003 <adikasultana88m@gmail.com>
2026-04-18 21:36:12 +0600

    Update jobs admin.py UN38

--- unisphere_project/jobs/admin.py
+++ unisphere_project/jobs/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import JobListing
+@admin.register(JobListing)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'company_name', 'job_type', 'posted_by', 'is_verified', 'application_deadline']
+    list_filter = ['is_verified', 'job_type']

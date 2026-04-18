f9f78a6797fea6c6a0f3413d92ecc1ac4704b09d
Adika2003 <adikasultana88m@gmail.com>
2026-04-18 21:26:14 +0600

    Update materials urls.pyUN-6

--- unisphere_project/materials/urls.py
+++ unisphere_project/materials/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = 'materials'
 
 urlpatterns = [
-    path('', views.material_list, name='list'),
+    path('', views.material_semesters, name='semesters'),
+
+    path('semester/<str:semester>/', views.material_courses_by_semester, name='semester_courses'),
+    path('semester/<str:semester>/course/<str:course_name>/', views.materials_by_course, name='course_materials'),
+
     path('create/', views.material_create, name='create'),
     path('pending/', views.material_pending, name='pending'),
+
     path('<int:pk>/', views.material_detail, name='detail'),
     path('<int:pk>/edit/', views.material_edit, name='edit'),
     path('<int:pk>/delete/', views.material_delete, name='delete'),

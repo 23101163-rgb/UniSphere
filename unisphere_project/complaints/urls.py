22ba677c0fb6f8d1ae16c7b3a310d9be85873009
shifat35 <23101142@uap-bd.edu>
2026-04-25 17:22:00 +0600

    Update complaints/urls.py

--- unisphere_project/complaints/urls.py
+++ unisphere_project/complaints/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'complaints'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('submit/', views.complaint_submit, name='submit'),
+    path('my/', views.my_complaints, name='my_complaints'),
+    path('<int:pk>/', views.complaint_detail, name='detail'),
+    path('manage/', views.complaint_manage, name='manage'),
+    path('teacher-manage/', views.teacher_complaint_manage, name='teacher_manage'),
+    path('<int:pk>/respond/', views.complaint_respond, name='respond'),
 ]
